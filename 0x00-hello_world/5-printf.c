a28833749e4f8defb633258590c5c0b8eb8f732c
Akhigbepeace <akhigbepeace2005@gmail.com>
2023-06-19 05:36:39 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+	*main - Entry point
+	*description - 'Print text using printf function'
+	*Return: Always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("\n");
+
+	return (0);
+}
